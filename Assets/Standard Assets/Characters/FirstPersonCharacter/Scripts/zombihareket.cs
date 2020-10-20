9ac12af7d8e67c8b147b2074169c9ad269f1dd29
nurabdullah <abdullahnur.ybs@gmail.com>
2020-10-20 16:11:27 +0300

    oyun tamamlandı

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/zombihareket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class zombihareket : MonoBehaviour
+{
+    private GameObject oyuncu;
+    void Start()
+    {
+        oyuncu = GameObject.Find("FPSController");
+    }
+
+    
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = oyuncu.transform.position;
+    }
+}
